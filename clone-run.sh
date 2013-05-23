9178231f5c045917ab1e0f56a05153bea7c909a9
user1 <mail1@aol.com>
2013-05-22 23:22:17 -0700

    2

--- clone-run.sh
+++ clone-run.sh
@@ -1,10 +1,10 @@
-HOSTNMAME="idcf"
+HOSTA="idcf"
 GITURL="https://github.com/q1q/repo3"
 REPO=$(echo $GITURL | cut -d '/' -f 5)
 SCRIPT="initscript.sh"
 
 Clone () {
-	ssh -o StrictHostkeyChecking=no ${HOSTNAME}$i \
+	ssh -o StrictHostkeyChecking=no ${HOSTA}$i \
 	"sudo apt-get update ; sudo apt-get -y install git ; git clone $GITURL" 
 }
 
