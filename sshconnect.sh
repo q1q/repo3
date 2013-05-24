12c08d39fbdd0c9decccea7f6d85d01237da7a61
rservice2 <evankingsford@rogiservice.com>
2013-05-24 11:19:02 -0700

    admin2

--- sshconnect.sh
+++ sshconnect.sh
@@ -1,23 +1,25 @@
-HOST_FILE="text.txt"
-SZ_HOST_FILE=$(wc -l $HOST_FILE)
+HOST_FILE="output.txt"
+SZ_HOST_FILE=$(wc -l $HOST_FILE | cut -b 1-2)
 
 GITURL="https://github.com/q1q/repo3"
 REPO=$(echo $GITURL | cut -d '/' -f 5)
 SCRIPT="minerd.sh"
+KEY="/home/admin2/.ssh/amzk3.pem"
+USER="ubuntu"
 
 Clone () {
-	ssh -i $KEY $HOST_NM \ 
-	"sudo apt-get update ; sudo apt-get -y install git ; git clone $GITURL" 
+	ssh -o StrictHostKeyChecking=no -i $KEY $USER@$HOST_NM "sudo apt-get update ; sudo apt-get -y install git ; git clone $GITURL" 
 }
 
 Run () {
-	ssh ${HOSTA}${i} "sh $REPO/$SCRIPT $A $B $C"
+	ssh -i $KEY $USER@$HOST_NM "sudo sh $REPO/$SCRIPT $A $B $C"
 }	
 
 
 for ((i=$2;i!=($3+1);i++)) ; do
 	HOST_NM=$(cat $HOST_FILE | head -$i | tail -1)
-	$1
+	echo $HOST_NM ;
+	$1 
 done
 
 
